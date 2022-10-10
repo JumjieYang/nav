5816d64d87040dce3714f27de2fecefaa6cdee2c
Junjie Yang <junjie.yang2@mail.mcgill.ca>
2022-10-10 08:06:50 -0400

    Update root.go

--- cmd/root.go
+++ cmd/root.go
@@ -32,13 +32,9 @@ var rootCmd = &cobra.Command{
 	Use:   "nav",
 	Short: "A cli tool to quickly jump to the repo or website",
 	Long:  `Nav is a CLI tool written in go that empowers developers to switch different repos faster`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
+
 func Execute() {
 	err := rootCmd.Execute()
 	if err != nil {
@@ -47,12 +43,4 @@ func Execute() {
 }
 
 func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.nav.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
 }
